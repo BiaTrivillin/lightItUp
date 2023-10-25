9c50fdaced359affb3b1ccb828122831e7261ea7
BiaTrivillin <biatrivillin.code@gmail.com>
2023-10-25 11:50:32 -0300

    created animation light bulb and add arrow image

---
+++ index.js
@@ -0,0 +1,10 @@
+const buttonStart = document.querySelector("#button__start");
+const lightShadow = document.querySelector("#light");
+const arrow = document.querySelector("#arrow");
+
+function lightItUp() {
+    lightShadow.style.display = "block";
+    buttonStart.style.display = "none";
+    arrow.style.display = "block";
+}
+
